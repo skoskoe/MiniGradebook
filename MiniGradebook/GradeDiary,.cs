4152e4ebda39de48a1fc2f35fbbe73dbe53ad816
Your Name <you@example.com>
2025-12-18 12:15:12 +0200

    rdytui

---
+++ MiniGradebook/GradeDiary,.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiniDiaryApp
+{
+    class GradeDiary
+    {
+        private List<int> grades = new List<int>();
+
+        private const int MinGrade = 2;
+        private const int MaxGrade = 6;
+
+        public void AddGrade(int grade)
+        {
+            grades.Add(grade);
+            Console.WriteLine("added.");
+        }
+
+        public void PrintAllGrades()
+        {
+            Console.WriteLine("grades:");
+
+            for (int i = 0; i < grades.Count; i++)
+            {
+                Console.WriteLine($"g[{i}]={grades[i]}");
+            }
+        }
+
+        public void PrintAverage()
+        {
+            if (grades.Count == 0)
+            {
+                Console.WriteLine("avg=0");
+                return;
+            }
+
+            int sum = 0;
+
+            foreach (var grade in grades)
+            {
+                sum += grade;
+            }
+
+            double avg = (double)sum / grades.Count;
+            Console.WriteLine("avg=" + avg);
+        }
+
+        public void PrintStats()
+        {
+            int[] gradeCounters = new int[MaxGrade + 1];
+
+            foreach (var grade in grades)
+            {
+                if (grade >= MinGrade && grade <= MaxGrade)
+                {
+                    gradeCounters[grade]++;
+                }
+            }
+
+            for (int i = MinGrade; i <= MaxGrade; i++)
+            {
+                Console.WriteLine($"{i}: {gradeCounters[i]}");
+            }
+
+            int total = grades.Count;
+
+            if (total > 0)
+            {
+                int fails = gradeCounters[MinGrade];
+                Console.WriteLine("fail%=" + (fails * 100 / total));
+                Console.WriteLine("pass%=" + ((total - fails) * 100 / total));
+            }
+        }
+    }
+}
---
+++ MiniGradebook/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MiniDiaryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeDiary diary = new GradeDiary();
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                ShowMenu();
+                string choice = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddGrade(diary);
+                        break;
+
+                    case "2":
+                        diary.PrintAllGrades();
+                        break;
+
+                    case "3":
+                        diary.PrintAverage();
+                        break;
+
+                    case "4":
+                        diary.PrintStats();
+                        break;
+
+                    case "5":
+                        isRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("wrong");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("1 - add grade");
+            Console.WriteLine("2 - show grades");
+            Console.WriteLine("3 - average");
+            Console.WriteLine("4 - stats");
+            Console.WriteLine("5 - exit");
+            Console.Write("choice: ");
+        }
+
+        static void AddGrade(GradeDiary diary)
+        {
+            Console.Write("grade: ");
+            string gradeText = Console.ReadLine();
+
+            int grade = int.Parse(gradeText);
+            diary.AddGrade(grade);
+        }
+    }
+}
